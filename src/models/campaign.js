80dc87d8b376404550e0d0c9506a99df5c4b0aaa
katehrybkova <katiushka152701@gmail.com>
2019-07-29 10:01:16 +0300

    test

--- src/models/campaign.js
+++ src/models/campaign.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+var productSchema = new Schema({  name: String, id: String, weight: Number, calloriesPerWeight: Number });
+
 let campaignSchema = new Schema({
-    date: { type: Date, default: Date.now, required: true },
-    products: [{ name: String, id: String, weight: Number, calloriesPerWeight: Number }],
+    date: { type: String, default: Date.now, required: true },
+    products: [productSchema],
     campaignId: { type:  String, required: true , unique: true},
     userId: { type: String, required: true },
 });
